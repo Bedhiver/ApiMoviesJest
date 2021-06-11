8931692131fa99e642ce4734c3bd172a6e5a7d7e
Francisco Fernandez <francisco59553@gmail.com>
2021-06-11 20:10:12 +0200

    feat: init express with es6 and nodemon

---
+++ app.js
@@ -0,0 +1,28 @@
+import express, { json, urlencoded } from "express";
+import { join } from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import path from "path";
+import homeRouter from "./routes/home.js";
+import usersRouter from "./routes/users.js";
+import { createServer } from "http";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+var app = express();
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(join(__dirname, "public")));
+
+app.use("/", homeRouter);
+app.use("/users", usersRouter);
+
+var server = createServer(app);
+server.listen(3000, () => {
+  console.log("Server running on " + 3000);
+  console.log(`http://localhost:${3000}`);
+});
+
+export default app;
